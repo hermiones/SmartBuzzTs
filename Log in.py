b370dd5a8ea601796431face2efb747cdaf5e00d
hermiones <sudiptadiya20@gmail.com>
2021-07-05 20:23:35 +0530

    commit

--- Log in.py
+++ Log in.py
@@ -4,5 +4,5 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get("https://smartbuzz.angelbroking.com/")
 driver.find_element_by_xpath("//a[contains(text(),'Login / Register')]").click()
-driver.find_element_by_xpath("//body/div[1]/main[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/div[1]/input[1]").send_keys("sudipta@skaledesk.xyz")
+driver.find_element_by_xpath("//body/div[1]/main[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/div[1]/input[1]").send_keys("sudipta@scaledesk.xyz")
 driver.find_element_by_xpath("//input[@id='login-password']").send_keys("123456789")
